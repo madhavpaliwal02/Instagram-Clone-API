f5299a0c7ac6862a81ae26b9251730baa4fab4f0
Madhav Paliwal <madhavpaliwal02@gmail.com>
2023-06-20 11:03:05 +0530

    Implementing pending User API

--- src/main/java/com/insta/controller/UserController.java
+++ src/main/java/com/insta/controller/UserController.java
@@ -6,13 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-// import com.insta.dto.MessageResponse;
+import com.insta.dto.MessageResponse;
 import com.insta.entity.User;
 import com.insta.exception.UserException;
 import com.insta.serviceImpl.service.UserService;
@@ -62,29 +63,44 @@ public class UserController {
     }
 
     // Update a user
-    // @PutMapping()
-    // public ResponseEntity<User> updateUserHandler(@RequestHeader("Authorization")
-    // String token) {
-    // return null;
-    // }
+    @PutMapping("/update")
+    public ResponseEntity<User> updateUserHandler(@RequestBody User user, @RequestHeader("Authorization") String token)
+            throws UserException {
+        User reqUser = userService.findUserByProfile(token);
+        User updatedUser = userService.updateUserDetails(user, reqUser);
+        return new ResponseEntity<User>(updatedUser, HttpStatus.ACCEPTED);
+    }
 
     // Follow a user
-    // @PutMapping("/follow/{followUserId}")
-    // public ResponseEntity<MessageResponse> followUserHandler() {
-    // return null;
-    // }
+    @PutMapping("/follow/{followUserId}")
+    public ResponseEntity<MessageResponse> followUserHandler(@PathVariable("followUserId") Integer followUserId,
+            @RequestHeader("Authorization") String token) throws UserException {
+        User user = userService.findUserByProfile(token);
+
+        String msg = userService.followUser(user.getId(), followUserId);
+
+        MessageResponse res = new MessageResponse(msg);
+        return new ResponseEntity<MessageResponse>(res, HttpStatus.OK);
+    }
 
     // Unfollow a user
-    // @PutMapping("/unfollow/{unfollowUserId}")
-    // public ResponseEntity<MessageResponse> unfollowUserHandler() {
-    // return null;
-    // }
+    @PutMapping("/unfollow/{unfollowUserId}")
+    public ResponseEntity<MessageResponse> unfollowUserHandler(@PathVariable("unfollowUserId") Integer unfollowUserId,
+            @RequestHeader("Authorization") String token) throws UserException {
+        User user = userService.findUserByProfile(token);
+
+        String msg = userService.unfollowUser(user.getId(), unfollowUserId);
+
+        MessageResponse res = new MessageResponse(msg);
+        return new ResponseEntity<MessageResponse>(res, HttpStatus.OK);
+    }
 
     // Get a user's profile
-    // @GetMapping("/req")
-    // public ResponseEntity<MessageResponse>
-    // findUserProfileHandler(@RequestHeader("Authorization")){
-    // return null;
-    // }
+    @GetMapping("/req")
+    public ResponseEntity<User> findUserProfileHandler(@RequestHeader("Authorization") String token)
+            throws UserException {
+        User user = userService.findUserByProfile(token);
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
 
 }
